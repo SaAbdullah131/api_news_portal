24158d2995f2bf98ccd03886e4add518719a0715
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-03-02 18:30:18 +0600

    all news categories shows in ui

--- js/news.js
+++ js/news.js
@@ -8,27 +8,34 @@ const showCategories= data => {
     // capture categories container to append all the category links
     const categoriesContainer = document.getElementById("categories-container");
     data.news_category.forEach(singleCategory => {
-        console.log(singleCategory);
+        // console.log(singleCategory);
         //simple way
         const linkContainer = document.createElement('p');
         linkContainer.innerHTML= `
-        <a class="nav-link href="#"onclick="fetchCategoryNews('${singleCategory?.category_id}'),'${singleCategory?.category_name}')">${singleCategory?.category_name}</a>
-        `
+        <a class="nav-link"href="#"onclick="fetchCategoryNews('${singleCategory?.category_id}')">${singleCategory?.category_name}</a>
+        `;
         categoriesContainer.appendChild(linkContainer);
-        // advanced
+        // advanced 
         // categoriesContainer.innerHTML += `
         // <a class="nav-link href="#">${singleCategory?.category_name}</a>
         // `
-    
     });
 }
 // fetch all news available in a category
 
-const fetchCategoryNews= category_id=>{
-     console.log(category_id)
-    const url = `https://openapi.programming-hero.com/api/news/category/%7Bcategory_id%7D`;
+const fetchCategoryNews = category_id=>{
+      console.log(category_id)
+     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+     console.log(url)
     fetch(url)
     .then(res =>res.json())
-    .then(data => showAllNews())
+    .then(data => showAllNews(data));
 }
  
+ const showAllNews = (allNes) => {
+    allNes.data.forEach(news =>{
+        console.log(news);
+    }
+    )}
+// }
+// fetchCategories();
